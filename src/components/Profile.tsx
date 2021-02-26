fe6742f5553b93ee0a26e175b8027f75106c3e20
Leandro Finochio <lefinochio@hotmail.com>
2021-02-26 14:27:13 -0300

    First commit

---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengeContext';
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/lefinochio.png" alt="Leandro Finochio" />
+      <div>
+        <strong>Leandro Finochio</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level {level}
+        </p>
+      </div>
+    </div>
+  );
+}
